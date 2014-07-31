c2297489700771519750e8dd99c44f781b7af624
Nubis <nubis@woobiz.com.ar>
2014-07-31 12:24:13 -0300

    added module in settings

--- lib/bitex_bot/settings.rb
+++ lib/bitex_bot/settings.rb
@@ -1,19 +1,21 @@
 require 'settingslogic'
 
-class BitexBot::Settings < Settingslogic
-  def self.load_default
-    path = ARGV[0] || File.expand_path('bitex_bot_settings.yml', Dir.pwd)
-    unless FileTest.exists?(path)
-      sample_path = File.expand_path('../../../settings.yml.sample', __FILE__)
-      FileUtils.cp(sample_path, path)
-      puts "No settings found, I've created a new one with sample "\
-        "values at #{path}. Please go ahead and edit it before running this again."
-      exit 1
+module BitexBot
+  class Settings < Settingslogic
+    def self.load_default
+      path = ARGV[0] || File.expand_path('bitex_bot_settings.yml', Dir.pwd)
+      unless FileTest.exists?(path)
+        sample_path = File.expand_path('../../../settings.yml.sample', __FILE__)
+        FileUtils.cp(sample_path, path)
+        puts "No settings found, I've created a new one with sample "\
+          "values at #{path}. Please go ahead and edit it before running this again."
+        exit 1
+      end
+      source path
     end
-    source path
-  end
-  
-  def self.load_test
-    source File.expand_path('../../../settings.yml.sample', __FILE__)
+    
+    def self.load_test
+      source File.expand_path('../../../settings.yml.sample', __FILE__)
+    end
   end
 end
