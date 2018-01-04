e2c505083f40ab986d2e3b2006d084316a7b6b60
Juan Szymczak <juanangelsilva@hotmail.com.ar>
2018-01-04 14:00:49 -0300

    add subject to Robot notifier

--- lib/bitex_bot/robot.rb
+++ lib/bitex_bot/robot.rb
@@ -36,7 +36,7 @@ module BitexBot
       end
     end
     cattr_accessor :current_cooldowns do 0 end
-    
+
     # Trade constantly respecting cooldown times so that we don't get
     # banned by api clients.
     def self.run!
@@ -44,7 +44,7 @@ module BitexBot
       logger.info("Loading trading robot, ctrl+c *once* to exit gracefully.")
       self.cooldown_until = Time.now
       bot = new
-      
+
       while true
         start_time = Time.now
         next if start_time < cooldown_until
@@ -55,18 +55,18 @@ module BitexBot
         self.cooldown_until = start_time + current_cooldowns.seconds
       end
     end
-    
+
     def self.setup
       Bitex.api_key = Settings.bitex
       Bitex.sandbox = Settings.sandbox
       taker.setup(Settings)
     end
-  
+
     def self.with_cooldown(&block)
       result = block.call
       return result if test_mode
       self.current_cooldowns += 1
-      sleep 0.1 
+      sleep 0.1
       return result
     end
 
@@ -95,14 +95,14 @@ module BitexBot
       self.notify("#{e.class} - #{e.message}:\n\n#{e.backtrace.join("\n")}")
       sleep 120 unless self.class.test_mode
     end
-    
+
     def finalise_some_opening_flows
       [BuyOpeningFlow, SellOpeningFlow].each do |kind|
         flows = self.class.graceful_shutdown ? kind.active : kind.old_active
         flows.each{|flow| flow.finalise! }
       end
     end
-    
+
     def start_closing_flows
       [BuyClosingFlow, SellClosingFlow].each{|kind| kind.close_open_positions}
     end
@@ -110,7 +110,7 @@ module BitexBot
     def open_positions?
       OpenBuy.open.exists? || OpenSell.open.exists?
     end
-    
+
     def sync_closing_flows
       orders = with_cooldown{ BitexBot::Robot.taker.orders }
       transactions = with_cooldown{ BitexBot::Robot.taker.user_transactions }
@@ -121,27 +121,27 @@ module BitexBot
         end
       end
     end
-    
+
     def active_closing_flows?
       BuyClosingFlow.active.exists? || SellClosingFlow.active.exists?
     end
-    
+
     def start_opening_flows_if_needed
       if store.reload.hold?
         BitexBot::Robot.logger.debug("Not placing new orders because of hold")
         return
       end
-      
+
       if active_closing_flows?
         BitexBot::Robot.logger.debug("Not placing new orders, closing flows.")
         return
       end
-      
+
       if self.class.graceful_shutdown
         BitexBot::Robot.logger.debug("Not placing new orders, shutting down.")
         return
       end
-      
+
       recent_buying, recent_selling =
         [BuyOpeningFlow, SellOpeningFlow].collect do |kind|
           threshold = (Settings.time_to_live / 2).seconds.ago
@@ -152,19 +152,19 @@ module BitexBot
         BitexBot::Robot.logger.debug("Not placing new orders, recent ones exist.")
         return
       end
-      
+
       balances = with_cooldown{ BitexBot::Robot.taker.balance }
       profile = Bitex::Profile.get
-      
+
       total_usd = balances['usd_balance'].to_d + profile[:usd_balance]
       total_btc = balances['btc_balance'].to_d + profile[:btc_balance]
-      
+
       last_log = `tail -c 61440 #{Settings.log.try(:file)}` if Settings.log.try(:file)
-  
+
       store.update_attributes(taker_usd: balances['usd_balance'],
         taker_btc: balances['btc_balance'], log: last_log)
-      
-      if store.last_warning.nil? || store.last_warning < 30.minutes.ago 
+
+      if store.last_warning.nil? || store.last_warning < 30.minutes.ago
         if store.usd_warning && total_usd <= store.usd_warning
           notify("USD balance is too low, it's #{total_usd},"\
             "make it #{store.usd_warning} to stop this warning.")
@@ -189,7 +189,7 @@ module BitexBot
 
       order_book = with_cooldown{ BitexBot::Robot.taker.order_book }
       transactions = with_cooldown{ BitexBot::Robot.taker.transactions }
-      
+
       unless recent_buying
         BuyOpeningFlow.create_for_market(
           balances['btc_available'].to_d,
@@ -209,22 +209,22 @@ module BitexBot
           store)
       end
     end
-    
+
     def sync_opening_flows
       [SellOpeningFlow, BuyOpeningFlow].each{|o| o.sync_open_positions }
     end
-    
+
     def active_opening_flows?
       BuyOpeningFlow.active.exists? || SellOpeningFlow.active.exists?
     end
-    
-    def notify(message)
+
+    def notify(message, subj = 'Notice from your robot trader')
       self.class.logger.error(message)
       if Settings.mailer
         mail = Mail.new do
           from Settings.mailer.from
           to Settings.mailer.to
-          subject 'Notice from your robot trader'
+          subject subj
           body message
         end
         mail.delivery_method(Settings.mailer.delivery_method.to_sym,
