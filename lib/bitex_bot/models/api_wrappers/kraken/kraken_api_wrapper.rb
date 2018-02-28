05131b66d0ece5e4e4e0898de84083acf54bb247
Juan Szymczak <juanangelszymczak@gmail.com>
2018-02-28 13:52:08 -0300

    now returns DSL Structs

--- lib/bitex_bot/models/api_wrappers/kraken/kraken_api_wrapper.rb
+++ lib/bitex_bot/models/api_wrappers/kraken/kraken_api_wrapper.rb
@@ -7,52 +7,52 @@ class KrakenApiWrapper < ApiWrapper
   end
 
   def self.transactions
-    client.public.trades('XBTUSD')[:XXBTZUSD].reverse.collect do |t|
-      Hashie::Mash.new(tid: t[2].to_s, price: t[0], amount: t[1], date: t[2])
-    end
+    client.public.trades('XBTUSD')[:XXBTZUSD].reverse.map { |t| transaction_parser(t) }
   rescue NoMethodError => e
     retry
   end
 
   def self.order_book(retries = 20)
-    {
-      'bids' => book[:bids].collect { |b| [ b[0], b[1] ] },
-      'asks' => book[:asks].collect { |a| [ a[0], a[1] ] }
-    }
+    book = client.public.order_book('XBTUSD')[:XXBTZUSD]
+    OrderBook.new(
+      Time.now.to_i,
+      book[:bids].map { |bid| OrderSummary.new(bid[0], bid[1]) },
+      book[:asks].map { |ask| OrderSummary.new(ask[0], ask[1]) }
+    )
   rescue NoMethodError => e
     retry
   end
 
   def self.balance
-    balances = client.private.balance
-    sell_orders = open_orders.select { |o| o.type == :sell }
-    btc_reserved = sell_orders.collect { |o| o.amount - o.executed_amount }.sum
-    buy_orders = open_orders - sell_orders
-    usd_reserved = buy_orders.collect { |o| (o.amount - o.executed_amount) * o.price }.sum
-    {
-      'btc_balance' => balances['XXBT'].to_d,
-      'btc_reserved' => btc_reserved,
-      'btc_available' => balances['XXBT'].to_d - btc_reserved,
-      'usd_balance' => balances['ZUSD'].to_d,
-      'usd_reserved' => usd_reserved,
-      'usd_available' => balances['ZUSD'].to_d - usd_reserved,
-      'fee' => client.private.trade_volume(pair: 'XBTUSD')[:fees][:XXBTZUSD][:fee].to_d
-    }
+    BalanceSummary.new.tap do |summary|
+      open_orders = orders
+      balances = client.private.balance
+
+      sell_orders = open_orders.select { |o| o.type == :sell }
+      btc_reserved = sell_orders.collect { |o| o.amount - o.executed_amount }.sum
+      summary[:btc] = Balance.new(balances['XXBT'].to_d, btc_reserved, balances['XXBT'].to_d - btc_reserved)
+
+      buy_orders = open_orders - sell_orders
+      usd_reserved = buy_orders.collect { |o| (o.amount - o.executed_amount) * o.price }.sum
+      summary[:usd] = Balance.new(balances['ZUSD'].to_d, usd_reserved, balances['ZUSD'].to_d - usd_reserved)
+
+      summary[:fee] = client.private.trade_volume(pair: 'XBTUSD')[:fees][:XXBTZUSD][:fee].to_d
+    end
   rescue KrakenClient::ErrorResponse, Net::ReadTimeout => e
     retry
   end
 
-  def self.enough_order_size?(quantity, price)
-    (quantity * price) > MIN_AMOUNT
-  end
-
   def self.orders
-    KrakenOrder.open
+    KrakenOrder.open.map { |ko| order_parser(ko) }
   end
 
   # We don't need to fetch the list of transactions for Kraken
   def self.user_transactions
     []
+  end
+
+  def self.enough_order_size?(quantity, price)
+    (quantity * price) > MIN_AMOUNT
   end
 
   def self.send_order(type, price, quantity)
@@ -67,17 +67,29 @@ class KrakenApiWrapper < ApiWrapper
     KrakenOrder.amount_and_quantity(order_id, transactions)
   end
 
-  private
-
   def self.client
     @client ||= KrakenClient.load(@settings)
   end
 
-  def self.book
-    @book ||= client.public.order_book('XBTUSD')[:XXBTZUSD]
+  private
+
+  def self.order_parser(o)
+    Order.new(o.id, o.type, o.price.to_d, o.amount.to_d, DateTime.parse(o.datetime).to_time.to_i)
   end
 
-  def self.open_orders
-    @open_orders ||= KrakenOrder.open
+  def self.transaction_parser(t)
+    Transaction.new(t[2].to_s, t[0].to_d, t[1].to_d, t[2].to_i)
+  end
+
+  def self.balance_summary_parser(b)
+    BalanceSummary.new(
+      Balance.new(b['btc_balance'].to_d, b['btc_reserved'].to_d, b['btc_available'].to_d),
+      Balance.new(b['usd_balance'].to_d, b['usd_reserved'].to_d, b['usd_available'].to_d),
+      b['fee'].to_d
+    )
+  end
+
+  def self.order_parser(o)
+    Order.new(o.id, o.type, o.price, o.amount, o.datetime)
   end
 end
