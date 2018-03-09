749566e932648a88488eedd25333203542d31569
Juan Szymczak <juanangelszymczak@gmail.com>
2018-03-09 18:09:44 -0300

    syntax refactor and add spec

--- spec/models/api_wrappers/bitstamp_api_wrapper_spec.rb
+++ spec/models/api_wrappers/bitstamp_api_wrapper_spec.rb
@@ -36,9 +36,11 @@ describe BitstampApiWrapper do
   end
 
   it 'Sends User-Agent header' do
-    stub_request(:post, 'https://www.bitstamp.net/api/v2/balance/btcusd/')
+    stuff_stub = stub_request(:post, 'https://www.bitstamp.net/api/v2/balance/btcusd/')
       .with(headers: { 'User-Agent': BitexBot.user_agent })
+
     BitstampApiWrapper.balance rescue nil # we don't care about the response
+    expect(stuff_stub).to have_been_requested
   end
 
   it '#transactions' do
