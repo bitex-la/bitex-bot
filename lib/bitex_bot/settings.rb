e4661adec3bc956804ffd668a38270374fa797a3
Juan Szymczak <juanangelszymczak@gmail.com>
2018-03-16 13:51:31 -0300

    settings rubofactor

--- lib/bitex_bot/settings.rb
+++ lib/bitex_bot/settings.rb
@@ -16,31 +16,38 @@ module BitexBot
   end
 
   ##
-  # Documentation here!
+  # This class load settings file, else write a sample file.
   #
   class SettingsClass < ::Hashie::Mash
     include ::Hashie::Extensions::Mash::SymbolizeKeys
 
     def load_default
-      path = ARGV[0] || 'bitex_bot_settings.rb'
-      unless FileTest.exists?(path)
-        sample_path = File.expand_path('../../../settings.rb.sample', __FILE__)
-        FileUtils.cp(sample_path, path)
-        puts "No settings found, I've created a new one with sample "\
-          "values at #{path}. Please go ahead and edit it before running this again."
-        exit 1
-      end
+      path = ARGV[0] || 'bitex_botss_settings.rb'
+      show_sample(path) unless FileTest.exists?(path)
       load_settings(path)
     end
 
     def load_test
-      load_settings File.expand_path('../../../settings.rb.sample', __FILE__)
+      load_settings(sample_path)
     end
+
+    private
 
     def load_settings(path)
       file_settings = FileSettings.new
       file_settings.instance_eval(File.read(path), path, 1)
       merge!(file_settings)
+    end
+
+    def sample_path
+      File.expand_path('../../settings.rb.sample', __dir__)
+    end
+
+    def show_sample(path)
+      FileUtils.cp(sample_path, path)
+      puts "No settings found, I've created a new one with sample values at #{path}. "\
+        'Please go ahead and edit it before running this again.'
+      exit 1
     end
   end
 
