2c36c86d25b9d7cff04c178349f75947772693f7
Juan Szymczak <juanangelszymczak@gmail.com>
2018-04-10 15:03:41 -0300

    fix arguments for method missing

--- lib/bitex_bot/models/api_wrappers/api_wrapper.rb
+++ lib/bitex_bot/models/api_wrappers/api_wrapper.rb
@@ -20,7 +20,7 @@ class ApiWrapper
     :order # Actual order object
   ) do
     def method_missing(method_name, *args, &block)
-      order.send(method_name, args, block) || super
+      order.send(method_name, *args, &block) || super
     end
 
     def respond_to_missing?(method_name, include_private = false)
