1c2ad119a373a3c920c5103963a452b254fe540c
Juan Szymczak <juanangelszymczak@gmail.com>
2018-04-10 15:53:10 -0300

    Use nested module/class definitions instead of compact style.

--- lib/bitex_bot/models/open_sell.rb
+++ lib/bitex_bot/models/open_sell.rb
@@ -2,9 +2,11 @@
 # OpenSells are open sell positions that are closed by one SellClosingFlow.
 # TODO: document attributes.
 #
-class BitexBot::OpenSell < ActiveRecord::Base
-  belongs_to :opening_flow, class_name: 'SellOpeningFlow', foreign_key: :opening_flow_id
-  belongs_to :closing_flow, class_name: 'SellClosingFlow', foreign_key: :closing_flow_id
+module BitexBot
+  class OpenSell < ActiveRecord::Base
+    belongs_to :opening_flow, class_name: 'SellOpeningFlow', foreign_key: :opening_flow_id
+    belongs_to :closing_flow, class_name: 'SellClosingFlow', foreign_key: :closing_flow_id
 
-  scope :open, -> { where('closing_flow_id IS NULL') }
+    scope :open, -> { where('closing_flow_id IS NULL') }
+  end
 end
