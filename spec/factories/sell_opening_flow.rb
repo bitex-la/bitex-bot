18f353ea8462244d6e0f6f44ec5d15f858c3159c
Juan Szymczak <juanangelszymczak@gmail.com>
2018-05-11 17:34:17 -0300

    pretty factories

--- spec/factories/sell_opening_flow.rb
+++ spec/factories/sell_opening_flow.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :sell_opening_flow, class: BitexBot::SellOpeningFlow do
-    price 300.0
-    value_to_use 2.0
-    suggested_closing_price 290.0
-    status 'executing'
-    order_id 12345
+    order_id                12_345
+    price                   300.to_d
+    value_to_use            2.to_d
+    suggested_closing_price 290.to_d
+    status                  'executing'
   end
 
   factory :other_sell_opening_flow, class: BitexBot::SellOpeningFlow do
-    price 400.0
-    value_to_use 1.0
-    suggested_closing_price 390.0
-    status 'executing'
-    order_id 2
+    order_id                2
+    price                   400.to_d
+    value_to_use            1.to_d
+    suggested_closing_price 390.to_d
+    status                  'executing'
   end
 end
