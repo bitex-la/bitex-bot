9e33bbdf4dee9153b907f34208fb70554832b081
Juan Szymczak <juanangelszymczak@gmail.com>
2018-06-05 18:12:59 -0300

    Revert "fix stub"
    This reverts commit 346c41bdc8462dabf044782aecc2db39abe56d17.

--- spec/models/buy_closing_flow_spec.rb
+++ spec/models/buy_closing_flow_spec.rb
@@ -227,9 +227,13 @@ describe BitexBot::BuyClosingFlow do
       BitexBot::Robot.setup
 
       BitstampApiWrapper.stub(send_order: nil)
+      counter = 0
+      stub_orders
 
-      Bitstamp.orders.stub(:all) do
-        [Bitstamp::Order.new(id: 1, type: 1, price: 310.to_s, amount: 2.5.to_s, datetime: 10.minutes.ago.strftime('%Y-%m-%d %H:%m:%S'))]
+      BitstampApiWrapper.stub(:find_lost) do |type, price, quantity|
+        counter += 1
+        next if counter < 3
+        BitstampApiWrapper.orders.first
       end
 
       open = create :open_buy
