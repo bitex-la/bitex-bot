648c5e67fdc61380b67e75b9541c1534c97993fc
Juan Szymczak <juanangelszymczak@gmail.com>
2018-07-10 14:12:08 -0300

    unify stop/warning flags

--- lib/bitex_bot/robot.rb
+++ lib/bitex_bot/robot.rb
@@ -183,10 +183,8 @@ module BitexBot
 
       check_balance_warning if expired_last_warning?
 
-      return log(:debug, "Not placing new orders, #{Settings.quote} target not met") if alert?(:maker, :fiat, :stop)
-      return log(:debug, "Not placing new orders, #{Settings.base} target not met") if alert?(:maker, :crypto, :stop)
-      return log(:debug, 'Not placing new orders, USD target not met') if alert?(:taker, :fiat, :stop)
-      return log(:debug, "Not placing new orders, #{Settings.quote} target not met") if alert?(:taker, :crypto, :stop)
+      return log(:debug, "Not placing new orders, #{Settings.quote} target not met") if alert?(:fiat, :stop)
+      return log(:debug, "Not placing new orders, #{Settings.base} target not met") if alert?(:crypto, :stop)
 
       order_book = with_cooldown { Robot.taker.order_book }
       transactions = with_cooldown { Robot.taker.transactions }
@@ -218,18 +216,19 @@ module BitexBot
       store.last_warning.nil? || store.last_warning < 30.minutes.ago
     end
 
-    # rubocop:disable Metrics/AbcSize
     def check_balance_warning
-      notify_balance_warning(Settings.base, store.maker_crypto, store.maker_crypto_warning) if alert?(:maker, :crypto, :warning)
-      notify_balance_warning(Settings.quote, store.maker_fiat, store.maker_fiat_warning) if alert?(:maker, :fiat, :warning)
-      notify_balance_warning(Settings.base, store.taker_crypto, store.taker_crypto_warning) if alert?(:taker, :crypto, :warning)
-      notify_balance_warning(:usd, store.taker_fiat, store.taker_fiat_warning) if alert?(:taker, :fiat, :warning)
+      notify_balance_warning(Settings.base, balance(:crypto), store.crypto_warning) if alert?(:crypto, :warning)
+      notify_balance_warning(Settings.quote, balance(:fiat), store.fiat_warning) if alert?(:fiat, :warning)
     end
-    # rubocop:enable Metrics/AbcSize
 
-    def alert?(market, currency, flag)
-      return unless store.send("#{market}_#{currency}_#{flag}").present?
-      store.send("#{market}_#{currency}") <= store.send("#{market}_#{currency}_#{flag}")
+    def alert?(currency, flag)
+      return unless store.send("#{currency}_#{flag}").present?
+      balance(currency) <= store.send("#{currency}_#{flag}")
+    end
+
+    def balance(currency)
+      fx_rate = currency == :fiat ? Settings.buying_fx_rate : 1
+      store.send("maker_#{currency}") / fx_rate + store.send("taker_#{currency}")
     end
 
     def notify_balance_warning(currency, amount, warning_amount)
