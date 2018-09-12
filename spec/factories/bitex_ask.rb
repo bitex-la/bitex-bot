618625f67970bd4d3fdf0de1c30f90296f1261d0
Juan Szymczak <juanangelszymczak@gmail.com>
2018-09-12 12:28:19 -0300

    Please use dynamic attributes instead by wrapping the attribute value in a block

--- spec/factories/bitex_ask.rb
+++ spec/factories/bitex_ask.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :bitex_ask, class: Bitex::Ask do
-    id                 12_345_678
-    created_at         Time.now
-    order_book         :btc_usd
-    price              1_000.to_d
-    status             :received
-    reason             :not_cancelled
-    issuer             'User#1'
-    quantity           100.to_d
-    remaining_quantity 100.to_d
-    produced_amount    10.to_d
+    id                 { 12_345_678 }
+    created_at         { Time.now }
+    order_book         { :btc_usd }
+    price              { 1_000.to_d }
+    status             { :received }
+    reason             { :not_cancelled }
+    issuer             { 'User#1' }
+    quantity           { 100.to_d }
+    remaining_quantity { 100.to_d }
+    produced_amount    { 10.to_d }
   end
 end
--- spec/factories/bitex_bid.rb
+++ spec/factories/bitex_bid.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :bitex_bid, class: Bitex::Bid do
-    id                12_345_678
-    created_at        Time.now
-    order_book        :btc_usd
-    price             1_000.to_d
-    status            :received
-    reason            :not_cancelled
-    issuer            'User#1'
-    amount            100.to_d
-    remaining_amount  100.to_d
-    produced_quantity 10.to_d
+    id                { 12_345_678 }
+    created_at        { Time.now }
+    order_book        { :btc_usd }
+    price             { 1_000.to_d }
+    status            { :received }
+    reason            { :not_cancelled }
+    issuer            { 'User#1' }
+    amount            { 100.to_d }
+    remaining_amount  { 100.to_d }
+    produced_quantity { 10.to_d }
   end
 end
