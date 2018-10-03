fb6d4d4357dcc62f830074bf35e4b121f8127546
Juan Szymczak <juanangelszymczak@gmail.com>
2018-10-03 15:14:20 -0300

    set attributes into dinamyc block

--- spec/factories/open_buy.rb
+++ spec/factories/open_buy.rb
@@ -2,18 +2,18 @@ FactoryBot.define do
   factory :open_buy, class: BitexBot::OpenBuy do
     association :opening_flow, factory: :buy_opening_flow 
 
-    transaction_id 12345678
-    price          300
-    amount         600
-    quantity       2
+    transaction_id { 12345678 }
+    price          { 300 }
+    amount         { 600 }
+    quantity       { 2 }
   end
 
   factory :tiny_open_buy, class: BitexBot::OpenBuy do
     association :opening_flow, factory: :other_buy_opening_flow 
 
-    transaction_id 23456789
-    price          400
-    amount         4
-    quantity       0.01
+    transaction_id { 23456789 }
+    price          { 400 }
+    amount         { 4 }
+    quantity       { 0.01 }
   end
 end
--- spec/factories/sell_opening_flow.rb
+++ spec/factories/sell_opening_flow.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :sell_opening_flow, class: BitexBot::SellOpeningFlow do
-    price 300.0
-    value_to_use 2.0
-    suggested_closing_price 290.0
-    status 'executing'
-    order_id 12345
+    price                   { 300.0 }
+    value_to_use            { 2.0 }
+    suggested_closing_price { 290.0 }
+    status                  { 'executing' }
+    order_id                { 12_345 }
   end
 
   factory :other_sell_opening_flow, class: BitexBot::SellOpeningFlow do
-    price 400.0
-    value_to_use 1.0
-    suggested_closing_price 390.0
-    status 'executing'
-    order_id 2
+    price                   { 400.0 }
+    value_to_use            { 1.0 }
+    suggested_closing_price { 390.0 }
+    status                  { 'executing' }
+    order_id                { 2 }
   end
 end
