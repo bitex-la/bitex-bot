ae1ecb4a1d73e078fb0ee28128f71cfb5c8185dd
Juan Szymczak <juanangelszymczak@gmail.com>
2018-12-31 21:34:54 -0300

    query scope refactor

--- lib/bitex_bot/models/open_buy.rb
+++ lib/bitex_bot/models/open_buy.rb
@@ -1,12 +1,10 @@
 module BitexBot
   # An OpenBuy represents a Buy transaction on Bitex.
   # OpenBuys are open buy positions that are closed by one or several CloseBuys.
-  # TODO: document attributes.
-  #
   class OpenBuy < ActiveRecord::Base
     belongs_to :opening_flow, class_name: 'BuyOpeningFlow', foreign_key: :opening_flow_id
     belongs_to :closing_flow, class_name: 'BuyClosingFlow', foreign_key: :closing_flow_id
 
-    scope :open, -> { where('closing_flow_id IS NULL') }
+    scope :open, -> { where(closing_flow: nil) }
   end
 end
