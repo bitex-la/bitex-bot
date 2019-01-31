c6769effcc6eb9e63dab5d0f8e26bdc89647dccc
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-31 10:46:34 -0300

    add Bitex sdk ruby dependency

--- spec/factories/bitex_ask.rb
+++ spec/factories/bitex_ask.rb
@@ -1,14 +1,11 @@
 FactoryBot.define do
-  factory :bitex_ask, class: Bitex::Ask do
-    id                 { 12_345_678 }
-    created_at         { Time.now }
-    order_book         { :btc_usd }
-    price              { 1_000.to_d }
-    status             { :received }
-    reason             { :not_cancelled }
-    issuer             { 'User#1' }
-    quantity           { 100.to_d }
-    remaining_quantity { 100.to_d }
-    produced_amount    { 10.to_d }
+  factory :bitex_ask, class: Bitex::Resources::Orders::Ask do
+    id                 { '12345678' }
+    type               { 'asks' }
+    orderbook_code     { 'btc_usd' }
+    amount             { '100' }
+    remaining_amount   { '100' }
+    price              { '1000' }
+    status             { 'received' }
   end
 end
--- spec/factories/bitex_bid.rb
+++ spec/factories/bitex_bid.rb
@@ -1,14 +1,11 @@
 FactoryBot.define do
-  factory :bitex_bid, class: Bitex::Bid do
-    id                { 12_345_678 }
-    created_at        { Time.now }
-    order_book        { :btc_usd }
-    price             { 1_000.to_d }
-    status            { :received }
-    reason            { :not_cancelled }
-    issuer            { 'User#1' }
-    amount            { 100.to_d }
-    remaining_amount  { 100.to_d }
-    produced_quantity { 10.to_d }
+  factory :bitex_bid, class: Bitex::Resources::Orders::Bid do
+    id                 { '12345678' }
+    type               { 'asks' }
+    orderbook_code     { 'btc_usd' }
+    amount             { '100' }
+    remaining_amount   { '100' }
+    price              { '1000' }
+    status             { 'received' }
   end
 end
