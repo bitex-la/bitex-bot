477260dcc8c898d7ed2d4c061051b37e760dfe2d
nubis <yo@nubis.im>
2019-06-11 16:08:10 -0300

    Removes racey contrived specs for buy opening flow specs

--- spec/features/buy_opening_flow_spec.rb
+++ spec/features/buy_opening_flow_spec.rb
@@ -46,32 +46,23 @@ describe BitexBot::BuyOpeningFlow do
         expect(flow.price.round(14)).to eq('19.85074626865672'.to_d)
       end
 
-      context 'finalising flow cancels associated bid' do
-        subject(:flow) { create(:buy_opening_flow, order_id: order.id) }
+      it 'cancels associated bid when finalising a flow' do
+        order = maker.send_order(:buy, 20, 50)
+        flow = create(:buy_opening_flow, order_id: order.id)
+        expect(order.status).to eq(:executing)
 
-        let(:order) { maker.send_order(:buy, 20, 50) }
+        expect(flow).to be_executing
 
-        it { expect(order.status).to eq(:executing) }
-        it { is_expected.to be_executing }
+        # On the first call we try to cancel the order.
+        flow.finalise!
 
-        context 'cancel one time' do
-          before(:each) { 
-            BitexBot::Notifier.logger.debug("Finalise #58")
-            flow.finalise!
-          }
+        expect(order.status).to eq(:cancelled)
+        expect(flow).to be_settling
 
-          it { expect(order.status).to eq(:cancelled) }
-          it { is_expected.to be_settling }
-
-          context 'cancels one more time' do
-            before(:each) {
-              BitexBot::Notifier.logger.debug("Finalise #68")
-              flow.finalise!
-            }
-
-            it { is_expected.to be_finalised }
-          end
-        end
+        # Second call will acknowledge order is cancelled, so it's finalised.
+        flow.finalise!
+        expect(order.status).to eq(:cancelled)
+        expect(flow).to be_finalised
       end
 
       context 'prioritizes profit from store' do
